efaeb5c83a1bc6838651f71b3b84e4cd0a7d57c6
Antonio Gonçalves Dias <antonio_g_d@hotmail.com>
2019-01-03 23:51:07 -0200

    First commit

---
+++ src/main/java/java/learning/Variables.java
@@ -0,0 +1,41 @@
+package java.learning;
+
+public class Variables {
+
+	public static void main( String[] args )
+    {
+        /* Comentário daqui,
+        até aqui */
+		
+		// uma linha de comentário sobre a idade
+		
+		// declara a idade
+		int age;
+		age = 29;
+		
+		// imprime a idade
+		System.out.println(age);
+		
+		// calcula a idade no ano seguinte
+		int ageNextYear;
+		ageNextYear = age + 1;
+		
+		int four = 2 + 2;
+		int three = 5 - 2;
+		
+		int eight = 4 * 2;
+		int sixteen = 64 / 4;
+		
+		int one = 5 % 2; 	// 5 dividido por 2 dá 2 e tem resto 1;	
+							// o operador % pega o resto da divisão inteira
+		
+		double pi = 3.14;
+		double x = 5 * 10;
+		
+		int age30 = 30;
+		boolean	minor = age30 < 18;
+		
+		char letter = 'a';
+		System.out.println(letter);
+    }
+}
