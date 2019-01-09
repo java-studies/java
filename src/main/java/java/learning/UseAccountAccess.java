3a500b42f32e78daf885d19d4e9d58ef75ea5083
Antonio Gonçalves Dias <antonio_g_d@hotmail.com>
2019-01-08 23:26:08 -0200

    Use attribute access

--- src/main/java/java/learning/UseAccountAccess.java
+++ src/main/java/java/learning/UseAccountAccess.java
@@ -6,5 +6,22 @@ public class UseAccountAccess {
 	
 	public static void main(String[] args) {
 		
+		Account account = new Account();
+		
+		account.setCodeNumber("1");
+		account.setNamePerson("Antonio G. Dias");
+		account.setMoney(999.50);
+		account.setStatus(true);
+		
+		System.out.println("----Account----");
+		System.out.println("Number: " + account.getCodeNumber());
+		System.out.println("Name of Person: " + account.getNamePerson());
+		System.out.println("Money: " + account.getMoney());
+		
+		if (account.isStatus()) {
+			System.out.println("Status: Active");
+		} else {
+			System.out.println("Status: Desactive");
+		}
 	}
 }
